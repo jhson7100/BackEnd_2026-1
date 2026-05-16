139f379f18f8d3fb220bb46651f4403d5903b210
son jeong han <aassws3213@gmail.com>
2026-05-16 17:08:05 +0900

    refactor: helloController 이름 변경

--- src/main/java/com/example/demo/IntroduceController.java
+++ src/main/java/com/example/demo/IntroduceController.java
@@ -5,16 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
+public class IntroduceController {
 
     @ResponseBody
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
-    }
-
-    @GetMapping("/hello2")
-    public String hello2() {
-        return "hello";
     }
 }
