f102d371aab297731403a5182cb46f0617cfb95e
son jeong han <aassws3213@gmail.com>
2026-05-18 16:48:16 +0900

    feat: /introduce?name 구현

--- src/main/java/com/example/demo/IntroduceController.java
+++ src/main/java/com/example/demo/IntroduceController.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IntroduceController {
 
     @GetMapping("/introduce")
-    public String introduce() {
+    public String introduce(
+            @RequestParam(name="name", required=false, defaultValue="손정한") String name,
+                             Model model) {
+        model.addAttribute("name", name);
         return "introduce";
     }
 }
