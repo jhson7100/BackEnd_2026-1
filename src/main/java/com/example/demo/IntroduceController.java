852c744db2aa115cd40cd84f4252268ecda68c2c
son jeong han <aassws3213@gmail.com>
2026-05-18 23:27:05 +0900

    feat: /json 구현

--- src/main/java/com/example/demo/IntroduceController.java
+++ src/main/java/com/example/demo/IntroduceController.java
@@ -4,6 +4,10 @@ import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Controller
 public class IntroduceController {
@@ -14,5 +18,16 @@ public class IntroduceController {
                              Model model) {
         model.addAttribute("name", name);
         return "introduce";
+    }
+
+    @ResponseBody
+    @GetMapping("/json")
+    public Map<String, Object> json() {
+        Map<String, Object> result = new LinkedHashMap<>();
+
+        result.put("name", "손정한");
+        result.put("age", 23);
+
+        return result;
     }
 }
