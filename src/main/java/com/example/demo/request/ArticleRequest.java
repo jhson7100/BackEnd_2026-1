a51a6cbef04bd5ccdebe118972472b8f386f0c15
son jeong han <aassws3213@gmail.com>
2026-05-25 17:18:59 +0900

    feat: ArticleRequest 추가 및 작성

---
+++ src/main/java/com/example/demo/request/ArticleRequest.java
@@ -0,0 +1,20 @@
+package com.example.demo.request;
+
+public class ArticleRequest {
+
+    private String title;
+    private String author;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
